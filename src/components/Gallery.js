b8d0304bcde40c45c778796995b53cec7bc28852
gretchelg <gretchelglopez@gmail.com>
2023-05-28 08:24:23 +0200

    added utils.js

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,5 +1,24 @@
+// import { getAllPokemons } from '../components/Utils'
+import anObject from '../components/Utils'
+import { useState, useEffect } from 'react';
+
 export default function Gallery() {
+    const [pokemons, setPokemons] = useState([]);
+
+    // useEffect(() => {
+    //     getAllPokemons();
+    // }, [])
+
+    anObject.fetchOne().then(data => setPokemons(data))
+    
+
     return (
+        <>
+        {/* <anObject.myFunc/> */}
+        <>{anObject.greeting}</>
+        <>{anObject.message}</>
         <div>Pokemon Gallery</div>
+        <>{JSON.stringify(pokemons)}</>
+        </>
     )
 }
