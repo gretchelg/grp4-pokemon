40df9bb31a1635dec9762b8f2281c569587084fb
Rohan Sapate <rhnsapate16@gmail.com>
2023-05-31 17:26:17 +0200

    leaderboard-changes

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -7,37 +7,37 @@ export default function Leaderboard() {
     const [users, setUsers] = useState([]);
 
     useEffect(() => {
-        fetchAPI.fetchAllUsers()
-        .then(res => { 
-            console.log("I'm in the Leaderboard:", res.data) 
-            setUsers(res.data)
-        })
-    }, [])
+      fetchAPI.fetchAllUsers()
+      .then(res => { 
+          console.log("I'm in the Leaderboard:", res.data) 
+          setUsers(res.data)
+      })
+  }, [])
 
-    return (
-      <div className="leaderboard-container">
-        <div className="header">
-          <button className="back-button">Back</button>
-          <h1 className="title">Leaderboard</h1>
-          <p className="score">Score: {users.score}</p>
-        </div>
-        {users.length > 0 && 
-        <div className="best-player">
-          <h2>Best Player</h2>
-          <div className="player">
-            <p className="player-name">{users.user_name}</p>
-            <p className="player-score">Score: {users.score}</p>
-          </div>
-          <p className="date">March 17</p>
-        </div>}
-        <div className="players-list">
-          {users.map((users, index) => (
-            <div key={index} className="player">
-              <p className="player-name">{users.user_name}</p>
-              <p className="player-score">Score: {users.score}</p>
-            </div>
-          ))}
-        </div>
+  return (
+    <div className="leaderboard-container">
+      <div className="header">
+        <button className="back-button">Back</button>
+        <h1 className="title">Leaderboard</h1>
+        {users.length > 0 && <p className="score">Score: {users[0].score}</p>}
       </div>
-    );
+      {users.length > 0 && 
+      <div className="best-player">
+        <h2>Best Player</h2>
+        <div className="player">
+          <p className="player-name">{users[0].user_name}</p>
+          <p className="player-score">Score: {users[0].score}</p>
+        </div>
+        <p className="date">March 17</p>
+      </div>}
+      <div className="players-list">
+        {users.map((user, index) => (
+          <div key={index} className="player">
+            <p className="player-name">{user.user_name}</p>
+            <p className="player-score">Score: {user.score}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
